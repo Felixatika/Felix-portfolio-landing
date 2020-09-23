69ac62f540573170a43afa463f92b5e08ace5ca4
Felixatika <felixatika@gmail.com>
2020-09-23 09:22:51 +0300

    First commit

---
+++ frontend/views/site/index.php
@@ -0,0 +1,12 @@
+<?php
+use yii\helpers\Url;
+/* @var $this yii\web\View */
+
+$this->title = 'Felix-portfolio-landing';
+?>
+<!-- <h1>Felix Portfolio Landing</h1>
+<a href="<?= Url::to(['site/about'])?>" class="btn-about">About Me</a> -->
+
+
+
+   
